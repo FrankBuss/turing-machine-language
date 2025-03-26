511c762715384aea7c6dab2e0243a7a29145997a
Frank Buss <fb@frank-buss.de>
2025-03-26 04:08:00 +0100

    regression test for Linux

---
+++ test/regression.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+echo "Running positive tests"
+
+sources=$(find . -name "*.tml" -type f -printf "%f\n")
+
+tests=0
+successes=0
+
+for source in $sources; do
+    ((tests++))
+    rm -f tmp.tm
+    rm -f tmp.output
+    ../compiler/compiler.exe "$source" tmp.tm
+    ../machine/machine.exe tmp.tm "${source}.input" tmp.output
+    if ! diff "${source}.output" tmp.output &>/dev/null; then
+        echo -e "\e[31mTest failed: $source\e[0m" >&2
+        diff "${source}.output" tmp.output
+        echo ""
+    else
+        echo "Test succeeded: $source"
+        ((successes++))
+    fi
+done
+
+echo "Running negative tests"
+
+sources=$(find . -name "*.neg" -type f -printf "%f\n")
+
+for source in $sources; do
+    ((tests++))
+    rm -f error
+    if ! ../compiler/compiler.exe "$source" tmp.tm 2>error; then
+        if grep -q "error" error; then
+            echo "Test succeeded: $source"
+            ((successes++))
+        else
+            echo -e "\e[31mTest failed: $source\e[0m" >&2
+        fi
+    else
+        echo -e "\e[31mTest failed: $source\e[0m" >&2
+    fi
+done
+
+echo "$successes out of $tests succeeded"
